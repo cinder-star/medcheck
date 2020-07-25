60f240a54d93cc9df1de26b31f2b7a01e9fbfe19
sihan-tawsik <sihantawsik@gmail.com>
2020-07-25 23:52:08 +0600

    fixed splash screen issue

--- app/src/main/java/com/axionesl/medcheck/activities/SplashActivity.kt
+++ app/src/main/java/com/axionesl/medcheck/activities/SplashActivity.kt
@@ -9,6 +9,8 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        startActivity(Intent(this, SignUpActivity::class.java))
+        val i = Intent(this, SignUpActivity::class.java)
+        i.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(i)
     }
 }
