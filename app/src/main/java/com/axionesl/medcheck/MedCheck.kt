7b951ee5f50a9ea39556b6aa04af6ddca8f88e01
sihan-tawsik <sihantawsik@gmail.com>
2020-07-26 15:57:00 +0600

    added annotation for application class

--- app/src/main/java/com/axionesl/medcheck/MedCheck.kt
+++ app/src/main/java/com/axionesl/medcheck/MedCheck.kt
@@ -3,6 +3,7 @@ package com.axionesl.medcheck
 import android.app.Application
 import io.paperdb.Paper
 
+@Suppress("unused")
 class MedCheck : Application() {
     override fun onCreate() {
         super.onCreate()
