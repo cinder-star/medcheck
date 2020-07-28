f8da1396009d07b7bc1f00fb4e04d31aabeac7cf
sihan-tawsik <sihantawsik@gmail.com>
2020-07-28 11:09:22 +0600

    added account persistence

--- app/src/main/java/com/axionesl/medcheck/activities/SplashActivity.kt
+++ app/src/main/java/com/axionesl/medcheck/activities/SplashActivity.kt
@@ -4,12 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.axionesl.medcheck.R
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        val i = Intent(this, LoginActivity::class.java)
+        if (checkUser()){
+            changeActivity(MainActivity::class.java)
+        } else {
+            changeActivity(LoginActivity::class.java)
+        }
+    }
+
+    private fun checkUser(): Boolean {
+        if (Firebase.auth.currentUser == null) return false
+        return true
+    }
+
+    private fun <T> changeActivity(jClass: Class<T>) {
+        val i = Intent(this, jClass)
         i.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         startActivity(i)
     }
