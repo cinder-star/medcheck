a1cd9f9ff0acb6866db3e4c267c6f56cb3047262
sihan-tawsik <sihantawsik@gmail.com>
2020-08-03 19:26:58 +0600

    added row view

--- app/src/main/java/com/axionesl/medcheck/domains/Test.kt
+++ app/src/main/java/com/axionesl/medcheck/domains/Test.kt
@@ -1,10 +1,14 @@
 package com.axionesl.medcheck.domains
 
 data class Test (
+    val id: String? = null,
     val weight: Double? = 0.0,
     val height: Double? = 0.0,
     val bpm: Double? = 0.0,
     val bloodPressure: Double? = 0.0,
     val glucoseLevel: Double? = 0.0,
-    val oxygenLevel: Double? = 0.0
+    val oxygenLevel: Double? = 0.0,
+    var status: String? = "In Queue",
+    var checkedBy: String? = null,
+    var patient: String? = null
 )
--- app/src/main/java/com/axionesl/medcheck/utils/PatientAdapter.kt
+++ app/src/main/java/com/axionesl/medcheck/utils/PatientAdapter.kt
@@ -1,24 +1,47 @@
 package com.axionesl.medcheck.utils
 
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.axionesl.medcheck.R
 import com.axionesl.medcheck.domains.Test
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
 
-class PatientAdapter(options: FirebaseRecyclerOptions<Test>) : FirebaseRecyclerAdapter<Test, PatientAdapter.ViewHolder>(
-    options
-) {
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-    }
+class PatientAdapter(private val context: Context, options: FirebaseRecyclerOptions<Test>) :
+    FirebaseRecyclerAdapter<Test, PatientAdapter.ViewHolder>(
+        options
+    ) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(context).inflate(R.layout.row_test, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int, model: Test) {
-        TODO("Not yet implemented")
+        holder.bindView(model)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindView(test: Test) {
+            val testId: TextView = itemView.findViewById(R.id.test_id)
+            val testStatus: TextView = itemView.findViewById(R.id.test_status)
+            val testCheckedBy: TextView = itemView.findViewById(R.id.test_checked_by)
+
+            testId.text = test.id
+            testStatus.text = test.status
+            if (test.checkedBy != null) {
+                testCheckedBy.text = test.checkedBy
+            }
+            if (test.status == "In Queue") {
+                testStatus.setTextColor(Color.parseColor("#FF0000"))
+            } else {
+                testStatus.setTextColor(Color.parseColor("#008000"))
+            }
+        }
     }
 }
