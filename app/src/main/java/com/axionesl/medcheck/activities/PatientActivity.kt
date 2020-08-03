0e488969f3ad0b3ef15e621954b67ebda0f07003
sihan-tawsik <sihantawsik@gmail.com>
2020-08-03 20:32:08 +0600

    added views

--- app/src/main/java/com/axionesl/medcheck/activities/PatientActivity.kt
+++ app/src/main/java/com/axionesl/medcheck/activities/PatientActivity.kt
@@ -1,11 +1,19 @@
 package com.axionesl.medcheck.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.axionesl.medcheck.R
+import com.axionesl.medcheck.domains.Test
+import com.axionesl.medcheck.utils.PatientAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class PatientActivity : AppCompatActivity() {
     private lateinit var addTest: FloatingActionButton
@@ -28,5 +36,23 @@ class PatientActivity : AppCompatActivity() {
         addTest.setOnClickListener {
             startActivity(Intent(this, AddTestActivity::class.java))
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        testList.layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
+        prepareRecyclerView()
+    }
+
+    private fun prepareRecyclerView() {
+        val uid = Firebase.auth.currentUser!!.uid
+        val query =
+            Firebase.database.reference.child("/tests/").orderByChild("patient").equalTo(uid)
+        val options = FirebaseRecyclerOptions.Builder<Test>()
+            .setQuery(query, Test::class.java)
+            .build()
+        val adapter = PatientAdapter(this, options)
+        testList.adapter = adapter
+        adapter.startListening()
     }
 }
