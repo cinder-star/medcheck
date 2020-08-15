640b9dbf1a6d9e40866cf50e859911abca16ea99
sihan-tawsik <sihantawsik@gmail.com>
2020-08-15 20:00:33 +0600

    completed profile activity

--- app/src/main/java/com/axionesl/medcheck/activities/ProfileActivity.kt
+++ app/src/main/java/com/axionesl/medcheck/activities/ProfileActivity.kt
@@ -6,12 +6,77 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.TextView
 import com.axionesl.medcheck.R
+import com.axionesl.medcheck.domains.User
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var name: TextView
+    private lateinit var email: TextView
+    private lateinit var accountType: TextView
+    private lateinit var mobileNumber: TextView
+    private lateinit var logOut: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        bindWidgets()
+        bindListeners()
+    }
+
+    private fun bindWidgets() {
+        name = findViewById(R.id.user_full_name)
+        email = findViewById(R.id.user_email)
+        mobileNumber = findViewById(R.id.user_mobile_number)
+        accountType = findViewById(R.id.user_account_type)
+        logOut = findViewById(R.id.log_out)
+    }
+
+    private fun bindListeners() {
+        logOut.setOnClickListener {
+            Firebase.auth.signOut()
+            changeActivity(LoginActivity::class.java)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val reference = Firebase
+            .database
+            .reference
+            .child("/user/")
+            .orderByChild("id")
+            .equalTo(Firebase.auth.currentUser!!.uid)
+        reference.keepSynced(true)
+        reference.addValueEventListener(object: ValueEventListener{
+            override fun onCancelled(error: DatabaseError) {}
+            override fun onDataChange(snapshot: DataSnapshot) {
+                snapshot.children.forEach {
+                    val user: User? = it.getValue<User>()
+                    updateUI(user)
+                }
+            }
+        })
+    }
+
+    private fun updateUI(user: User?) {
+        name.text = user!!.fullName
+        email.text = user.email
+        mobileNumber.text = user.mobileNumber
+        accountType.text = user.accountType
+    }
+
+    private fun <T> changeActivity(jClass: Class<T>) {
+        val i = Intent(this, jClass)
+        i.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(i)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
